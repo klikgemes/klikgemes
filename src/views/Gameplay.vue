a4f99e8dfca5fa8b81d0da51d0bcfa1ce8ef3649
aliyanamu <aliyanamu@gmail.com>
2018-09-21 09:54:37 +0700

    add baby

--- src/views/Gameplay.vue
+++ src/views/Gameplay.vue
@@ -7,75 +7,96 @@
         <div style="text-align: center">
           <img :src="user.avatar" alt=""> Name : <span>{{user.username}}</span>
         </div>
-        <div class="card" style="width: 16rem;">
+        <!-- <div class="card" style="width: 16rem;">
           <img class="card-img-top" src="https://svgsilh.com/svg/1299754-009688.svg" alt="Card image cap">
+        </div> -->
+        <div>
+          <baby-obj :id="user.id"></baby-obj>
         </div>
-        <button @click="clickMe(user.id)" v-if="!finish && gameStart && id == user.id">TEKAN AKU</button>
+        <button :id="user.id" @click="clickMe(user.id)" v-if="!finish && gameStart && id == user.id"></button>
       </div>
 
     </div>
-    <div id="absolutePosition" class="card w-75" style="position: absolute;" v-if="!gameStart">
+    <!-- <div id="absolutePosition" class="card w-75" style="position: absolute;" v-if="!gameStart">
       <div class="card-body">
         <h5 class="card-title">Mohon Tunggu</h5>
         <p class="card-text">Jumlah Pemain Tidak Cukup, Cepat Ajak Teman-Teman Mu</p>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-  import database from '../assets/config.js'
+import database from '../assets/config.js'
+import BabyObj from '@/components/BabyObj.vue'
 
-  export default {
-    name: 'gameplay',
-    data: function () {
-      return {
-        users: [],
-        finish: false,
-        gameStart: false,
-        id: ''
+export default {
+  name: 'gameplay',
+  components: {
+    BabyObj
+  },
+  data: function () {
+    return {
+      users: [],
+      finish: false,
+      gameStart: false,
+      id: ''
+    }
+  },
+  created: function () {
+    let self = this
+    var starCountRef = database.ref('room/' + localStorage.getItem('room') + '/users')
+    starCountRef.on('value', function (snapshot) {
+      self.id = localStorage.getItem('id')
+      self.users = []
+      let keys = []
+      let values = []
+      if (snapshot.val()) {
+        keys = Object.keys(snapshot.val())
+        values = Object.values(snapshot.val())
       }
-    },
-    created: function () {
-      
-      let self = this
-      var starCountRef = database.ref('room/' + localStorage.getItem('room') + '/users')
-      starCountRef.on('value', function (snapshot) {
-        self.id = localStorage.getItem('id')
-        self.users = []
-        let keys = []
-        let values = []
-        if (snapshot.val()) {
-          keys = Object.keys(snapshot.val())
-          values = Object.values(snapshot.val())
-        }
 
-        if (keys.length == 4) {
-          self.gameStart = true
-        }
+      if (keys.length == 4) {
+        self.gameStart = true
+      }
 
-        keys.forEach((key, index) => {
-          self.users.push({
-            id: key,
-            username: values[index].userName,
-            avatar: values[index].avatar
-          })
-          if (values[index].count >= 50) {
-            self.finish = true
-          }
+      keys.forEach((key, index) => {
+        self.users.push({
+          id: key,
+          username: values[index].userName,
+          avatar: values[index].avatar
+        })
+        if (values[index].count >= 50) {
+          self.finish = true
+        }
+      })
+    })
+  },
+  methods: {
+    clickMe: function (userId) {
+      var starCountRef = database.ref('room/' + localStorage.getItem('room') + '/users/' + userId)
+      starCountRef.once('value', function (snapshot) {
+        let counter = snapshot.val().count
+        starCountRef.update({
+          count: counter + 1
         })
       })
-    },
-    methods: {
-      clickMe: function (userId) {
-        var starCountRef = database.ref('room/' + localStorage.getItem('room') + '/users/' + userId)
-        starCountRef.once('value', function (snapshot) {
-          let counter = snapshot.val().count
-          starCountRef.update({
-            count: counter + 1
-          })
-        })
-      }
     }
   }
+}
 </script>
+
+<style scoped>
+button {
+  position: relative;
+  top: 400px;
+  border: none;
+  color: #fff;
+  width: 100px;
+  height: 100px;
+  transition: 0.3s;
+  left: 25%;
+  border-radius: 50%;
+  background-color: #f2ca27;
+}
+</style>
